00797a49bbe425a478091f3d75bbea03b4086c4d
jeanlain <jeanpeccoud@gmail.com>
2023-11-04 11:32:53 +0100

    Various adjustments to improve compatibly with macOS 14 and minor fixes and UI improvements

---
+++ STRyper/Categories/NSArray+NSArrayAdditions.h
@@ -0,0 +1,16 @@
+//
+//  NSArray+NSArrayAdditions.h
+//  STRyper
+//
+//  Created by Jean Peccoud on 27/10/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSArray (NSArrayAdditions)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ STRyper/Categories/NSArray+NSArrayAdditions.m
@@ -0,0 +1,12 @@
+//
+//  NSArray+NSArrayAdditions.m
+//  STRyper
+//
+//  Created by Jean Peccoud on 27/10/2023.
+//
+
+#import "NSArray+NSArrayAdditions.h"
+
+@implementation NSArray (NSArrayAdditions)
+
+@end
